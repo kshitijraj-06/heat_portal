bca2f6b4bd75bff6867e29048c97888535f74d75
kshit <kshitij6102004@gmail.com>
2025-04-29 22:28:27 +0530

    inital commit

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -7,6 +7,13 @@ class SplashScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text('HEAT'),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
     );
   }
 }
