e8a71ecc96dd450f2695106de2ef3a6c2919c7fb
kshit <kshitij6102004@gmail.com>
2025-05-06 00:58:30 +0530

    Added - edit_emp_service.dart & edit_employees.dart(basic ui)

--- lib/Screens/Admin_Screens/edit_employees.dart
+++ lib/Screens/Admin_Screens/edit_employees.dart
@@ -1,20 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:heat_portal/Models/user_model.dart';
+import 'package:heat_portal/WIdgets/appbar.dart';
 
-import '../../WIdgets/appbar.dart';
+import '../../Services/edit_emp_service.dart';
 
-class EditEmployee extends StatelessWidget{
-  const EditEmployee({super.key});
+class EditEmployeePage extends StatelessWidget {
+  final controller = Get.put(EditEmployeeController());
+  final String employeeId;
+  final User employee;
+
+  EditEmployeePage({required this.employeeId, required this.employee}) {
+    controller.loadData(employee);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: InkWell(
+      appBar: AppBar(title: GestureDetector(
           onTap: (){
             Get.offAllNamed('/dashboard');
           },
-          child: Appbar()
+          child: Appbar())),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            TextField(
+              controller: controller.idController,
+              decoration: InputDecoration(labelText: "ID"),
+            ),
+            TextField(
+              controller: controller.nameController,
+              decoration: InputDecoration(labelText: "Name"),
+            ),
+            TextField(
+              controller: controller.emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: controller.passwordController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 16),
+            Text("Select Roles"),
+            Obx(() => Wrap(
+              spacing: 10,
+              children: ['ADMIN', 'SALES', 'MANAGER']
+                  .map((role) => FilterChip(
+                label: Text(role),
+                selected: controller.selectedRoles.contains(role),
+                onSelected: (val) {
+                  val
+                      ? controller.selectedRoles.add(role)
+                      : controller.selectedRoles.remove(role);
+                },
+              ))
+                  .toList(),
+            )),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => controller.updatedata(employeeId),
+              child: Text("Update"),
+            ),
+          ],
         ),
       ),
     );
